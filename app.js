c0299540a3571ae3a721372e4ee4ee2c6f31c9b8
pranavjeet123 <pranavjeetmishra@gmail.com>
2020-05-27 23:23:29 +0530

    Adding Requirement of app

---
+++ app.js
@@ -0,0 +1,11 @@
+/**
+ * This script file will fulfill foloowing requirements
+ * 1- Adding a book
+ * 2- Check for any duplicate book and alert the user
+ * 3- Deletion of Book
+ * 4- Search of book
+ * 5- Displaying the book in a table Row wise(Default)
+ * 6- Both Grid display and Row display based on User selection.
+ * 7- Update/Edit with save option of the book
+ * 8- Accessing LocalStorge of Browser and Save the preference of User's Favorite book.
+ */
